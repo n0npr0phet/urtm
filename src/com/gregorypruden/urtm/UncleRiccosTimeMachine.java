7b186707a81555828f2d5b2286e7781d9f8eea8f
Gregory Pruden <gergory.pruden@gmail.com>
2009-10-08 01:44:05 -0400

    first commit

---
+++ src/com/gregorypruden/urtm/UncleRiccosTimeMachine.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.gregorypruden.urtm;
+
+/**
+ * @author gregory
+ *
+ */
+public class UncleRiccosTimeMachine {
+
+	/**
+	 * 
+	 */
+	public UncleRiccosTimeMachine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		header();
+	}
+	/**
+	 * header - prints the console output at the top of the program.
+	 */
+	public static void header()
+	{
+		System.out.println();
+		System.out.println("Uncle Rocco's Time Machine.");
+	}
+}
